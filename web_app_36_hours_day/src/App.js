52370cc81d84677e0f081a3646c593b3249eb7c7
Eng. Elias <eng.elias.owis@gmail.com>
2023-07-28 10:14:29 +0300

    adding some packages and fonts

--- web_app_36_hours_day/src/App.js
+++ web_app_36_hours_day/src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// roboto font
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+// digital7 font
+import "./fonts/digital7/digital-7.ttf";
+import "./fonts/digital7/digital-7 (italic).ttf";
+import "./fonts/digital7/digital-7 (mono italic).ttf";
+import "./fonts/digital7/digital-7 (mono).ttf";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{ fontFamily: "digital7" }}>
+          Edit <code>src/App.js</code> and save to reload. 12:00:00
         </p>
         <a
           className="App-link"
