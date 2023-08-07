dd7295490ae3ee22d8c637df50594d8f5dd6fe9a
Eng. Elias <eng.elias.owis@gmail.com>
2023-08-07 16:33:12 +0300

    first step to 36-hour-day: normal clock with moderm design

--- web_app_36_hours_day/src/App.js
+++ web_app_36_hours_day/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 // roboto font
@@ -12,24 +11,12 @@ import "./fonts/digital7/digital-7.ttf";
 import "./fonts/digital7/digital-7 (italic).ttf";
 import "./fonts/digital7/digital-7 (mono italic).ttf";
 import "./fonts/digital7/digital-7 (mono).ttf";
+import Clock from "./components/Clock";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{ fontFamily: "digital7" }}>
-          Edit <code>src/App.js</code> and save to reload. 12:00:00
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Clock />
     </div>
   );
 }
