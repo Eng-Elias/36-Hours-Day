8555f6a737be4f63c384db3336ac2c67da22d2ef
Eng. Elias <eng.elias.owis@gmail.com>
2023-09-13 14:59:51 +0300

    adding react-native-paper UI library and react-native-storage and async storage

--- mobile_app_36_hours_day/index.js
+++ mobile_app_36_hours_day/index.js
@@ -5,5 +5,14 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {MD3DarkTheme as DefaultTheme, PaperProvider} from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return (
+    <PaperProvider theme={DefaultTheme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
