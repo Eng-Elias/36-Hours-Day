945ca7708f56eb304dd9bd651ae385e2295a1c9a
Eng. Elias <eng.elias.owis@gmail.com>
2023-09-14 18:12:13 +0300

    adding digital clock component

--- mobile_app_36_hours_day/App.tsx
+++ mobile_app_36_hours_day/App.tsx
@@ -5,55 +5,19 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import {useEffect, useState} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  useColorScheme,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import TimePassed, {PassedTimeType} from './src/components/TimePassed';
+import DaysOf35HoursUtils from './src/utils/days_of_36_hours_utils';
+import Clock from './src/components/Clock';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -62,57 +26,55 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [startHour, setStartHour] = useState<number>(
+    DaysOf35HoursUtils.defaultStartHour,
+  );
+
+  useEffect(() => {
+    DaysOf35HoursUtils.getStartHourFromStorage()
+      .then(startHour => {
+        console.log(startHour);
+        if (typeof startHour === 'number') {
+          setStartHour(startHour);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  const [passedTime, setPassedTime] = useState<PassedTimeType>(null);
+
+  useEffect(() => {
+    let interval: string | number | NodeJS.Timeout | undefined;
+
+    function getTime() {
+      const lostTime =
+        DaysOf35HoursUtils.calculateTimeLostIn36HoursDay(startHour);
+      setPassedTime(lostTime);
+    }
+
+    interval = setInterval(getTime, 666);
+    getTime();
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [startHour]);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={{...backgroundStyle, flex: 1}}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View style={{flex: 2, justifyContent: 'space-between'}}>
+        <Clock passedTime={passedTime} />
+      </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
---
+++ mobile_app_36_hours_day/src/components/Clock.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {PassedTimeType} from './TimePassed';
+import {Card, Text, useTheme} from 'react-native-paper';
+
+function Clock({passedTime}: {passedTime: PassedTimeType}) {
+  const theme = useTheme();
+
+  if (!passedTime) return null;
+
+  return (
+    <View style={{margin: 16}}>
+      <Card>
+        <Card.Content
+          style={{
+            alignItems: 'center',
+            backgroundColor: '#3771e0',
+          }}>
+          <Text
+            style={{
+              fontFamily: 'digital-7 (mono)',
+              fontSize: 35,
+              color: theme.colors.scrim,
+            }}>
+            You have
+          </Text>
+          <Text
+            style={{
+              fontFamily: 'digital-7 (mono)',
+              fontSize: 70,
+              textAlign: 'center',
+              color: theme.colors.scrim,
+            }}>
+            {`${36 - passedTime.lostHours}:${60 - passedTime.lostMinutes}:${
+              60 - passedTime.lostSeconds
+            }`}
+          </Text>
+        </Card.Content>
+      </Card>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default Clock;
