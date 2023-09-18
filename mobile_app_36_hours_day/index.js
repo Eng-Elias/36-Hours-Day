6d1a016ca351e9eddea94d091146d5945defced0
Eng. Elias <eng.elias.owis@gmail.com>
2023-09-18 20:17:51 +0300

    adding react-native-navigation and react-native-reanimated to add drawer settings

--- mobile_app_36_hours_day/index.js
+++ mobile_app_36_hours_day/index.js
@@ -6,11 +6,14 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {MD3DarkTheme as DefaultTheme, PaperProvider} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
 
 export default function Main() {
   return (
     <PaperProvider theme={DefaultTheme}>
-      <App />
+      <NavigationContainer>
+        <App />
+      </NavigationContainer>
     </PaperProvider>
   );
 }
