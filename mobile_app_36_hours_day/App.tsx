bb3bb2dc5ea7859bd2f46c29325b03c383102196
Eng. Elias <eng.elias.owis@gmail.com>
2023-09-19 20:09:38 +0300

    remove unused imports

--- mobile_app_36_hours_day/App.tsx
+++ mobile_app_36_hours_day/App.tsx
@@ -1,23 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   StatusBar,
   StyleSheet,
-  View,
   useColorScheme,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {PassedTimeType} from './src/components/TimePassed';
-import DaysOf35HoursUtils from './src/utils/days_of_36_hours_utils';
-import TextToSpeechUtils from './src/utils/text_to_speech_utils';
 import SettingsDrawer from './src/components/SettingsDrawer';
 
 function App(): JSX.Element {
@@ -26,51 +14,6 @@ function App(): JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-
-  const [startHour, setStartHour] = useState<number>(
-    DaysOf35HoursUtils.defaultStartHour,
-  );
-
-  useEffect(() => {
-    DaysOf35HoursUtils.getStartHourFromStorage()
-      .then(startHour => {
-        console.log(startHour);
-        if (typeof startHour === 'number') {
-          setStartHour(startHour);
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-
-  const [passedTime, setPassedTime] = useState<PassedTimeType>(null);
-
-  useEffect(() => {
-    let interval: string | number | NodeJS.Timeout | undefined;
-
-    function getTime() {
-      const lostTime =
-        DaysOf35HoursUtils.calculateTimeLostIn36HoursDay(startHour);
-      setPassedTime(lostTime);
-
-      if (lostTime.lostMinutes === 0 && lostTime.lostSeconds === 0) {
-        const hourWord = lostTime.lostHours > 1 ? 'hours' : 'hour';
-        TextToSpeechUtils.speakTextWithDucking(`
-          ${lostTime.lostHours} ${hourWord} passed, you have ${
-          36 - lostTime.lostHours
-        } ${hourWord}.
-        `);
-      }
-    }
-
-    interval = setInterval(getTime, 666);
-    getTime();
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [startHour]);
 
   return (
     <SafeAreaView style={{...backgroundStyle, flex: 1}}>
