280407430d21f302cffa24ae61eefc9678fddca1
Eng. Elias <eng.elias.owis@gmail.com>
2023-10-02 18:25:00 +0300

    use Redux store to store and update startHour settings

--- mobile_app_36_hours_day/src/components/SettingsDrawer.tsx
+++ mobile_app_36_hours_day/src/components/SettingsDrawer.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
-import {IconButton, Drawer as PaperDrawer} from 'react-native-paper';
-import {Text, View} from 'react-native';
+import {
+  HelperText,
+  IconButton,
+  Drawer as PaperDrawer,
+  TextInput,
+} from 'react-native-paper';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeScreen from '../screens/HomeScreen';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import DaysOf35HoursUtils from '../utils/days_of_36_hours_utils';
+import store, {changeStartHour} from '../redux/store';
 
 const Drawer = createDrawerNavigator();
 
@@ -12,9 +18,63 @@ export const screens = {
 };
 
 function DrawerContent() {
+  const [text, setText] = React.useState(
+    store.getState().settings.startHour.toString(),
+  );
+  const [error, setError] = React.useState(false);
+
+  React.useEffect(() => {
+    DaysOf35HoursUtils.getStartHourFromStorage()
+      .then(startHour => {
+        console.log(startHour);
+        if (typeof startHour === 'number') {
+          store.dispatch(changeStartHour(startHour));
+          setText(startHour.toString());
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleTextChange = (input: string) => {
+    // Remove any non-numeric characters
+    const numericInput = input.replace(/[^0-9]/g, '');
+
+    const numericValue = parseInt(numericInput, 10);
+
+    // If input is empty or a valid integer between 0 and 23, update the state
+    if (
+      numericInput === '' ||
+      (!isNaN(numericValue) && numericValue >= 0 && numericValue <= 23)
+    ) {
+      setText(numericInput);
+      if (!isNaN(numericValue)) {
+        store.dispatch(changeStartHour(numericValue));
+        DaysOf35HoursUtils.storeStartHourInStorage(numericValue);
+      }
+      setError(false); // Clear the error if input is valid
+    } else {
+      setError(true); // Set error to true if input is invalid
+    }
+  };
+
   return (
     <PaperDrawer.Section title="Start Hour">
-      <Text>Drawer Content</Text>
+      <TextInput
+        label="Start Hour"
+        mode="outlined"
+        outlineColor="#3771e0"
+        activeOutlineColor="white"
+        textColor="#3771e0"
+        value={text}
+        onChangeText={handleTextChange}
+        inputMode="numeric"
+        keyboardType="numeric"
+      />
+      <HelperText type="error" visible={error}>
+        Please enter a valid integer between 0 and 23.
+      </HelperText>
     </PaperDrawer.Section>
   );
 }
--- mobile_app_36_hours_day/src/screens/HomeScreen.tsx
+++ mobile_app_36_hours_day/src/screens/HomeScreen.tsx
@@ -1,17 +1,16 @@
 import {View} from 'react-native';
-import {Text} from 'react-native-paper';
 import TimePassed, {PassedTimeType} from '../components/TimePassed';
 import Clock from '../components/Clock';
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import DaysOf35HoursUtils from '../utils/days_of_36_hours_utils';
 import TextToSpeechUtils from '../utils/text_to_speech_utils';
+import store from '../redux/store';
 
 class HomeScreen extends React.Component<
-  {passedTime: PassedTimeType},
-  {startHour: number; passedTime: PassedTimeType}
+  {route: any},
+  Readonly<{passedTime: PassedTimeType}>
 > {
-  state: Readonly<{startHour: number; passedTime: PassedTimeType}> = {
-    startHour: DaysOf35HoursUtils.defaultStartHour,
+  state: Readonly<{passedTime: PassedTimeType}> = {
     passedTime: null,
   };
 
@@ -23,9 +22,11 @@ class HomeScreen extends React.Component<
 
   runClockInterval() {
     const getTime = () => {
-      const lostTime = DaysOf35HoursUtils.calculateTimeLostIn36HoursDay(
-        this.state.startHour,
-      );
+      // console.log(store.getState().settings.startHour);
+      const startHour = store.getState().settings.startHour;
+
+      const lostTime =
+        DaysOf35HoursUtils.calculateTimeLostIn36HoursDay(startHour);
       this.setState({passedTime: lostTime});
 
       if (lostTime.lostMinutes === 0 && lostTime.lostSeconds === 0) {
@@ -42,20 +43,7 @@ class HomeScreen extends React.Component<
     getTime();
   }
 
-  updateStartHourFromLocalStorage() {
-    DaysOf35HoursUtils.getStartHourFromStorage()
-      .then(startHour => {
-        if (typeof startHour === 'number') {
-          this.setState({startHour});
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
   componentDidMount(): void {
-    this.updateStartHourFromLocalStorage();
     this.runClockInterval();
   }
 
