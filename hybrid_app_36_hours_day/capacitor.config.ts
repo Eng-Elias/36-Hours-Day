42285de2a22ced02d78ef03d94941a66c29878a9
Eng. Elias <eng.elias.owis@gmail.com>
2023-10-03 19:16:55 +0300

    Ionic hybrid mobile app

---
+++ hybrid_app_36_hours_day/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hybrid_app_36_hours_day',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
