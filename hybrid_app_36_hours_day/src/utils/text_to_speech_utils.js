69aa997b9079e333ac06581f348c9ecb4a63bfb0
Eng. Elias <eng.elias.owis@gmail.com>
2023-10-17 13:23:49 +0300

    apply ionic text to speach feature

--- hybrid_app_36_hours_day/src/utils/text_to_speech_utils.js
+++ hybrid_app_36_hours_day/src/utils/text_to_speech_utils.js
@@ -1,7 +1,33 @@
+import { TextToSpeech } from "@capacitor-community/text-to-speech";
+
 export default class TextToSpeechUtils {
-  static speakText(text) {
-    // Init SpeechSynth API
-    const synth = window.speechSynthesis;
-    synth.speak(new SpeechSynthesisUtterance(text));
+  static async speakText(text) {
+    await TextToSpeech.speak({
+      text: text,
+      lang: "en-US",
+      rate: 1.0,
+      pitch: 1.0,
+      volume: 1.0,
+      category: "ambient",
+    });
+  }
+
+  static async stop() {
+    await TextToSpeech.stop();
+  }
+
+  static async getSupportedLanguages() {
+    const languages = await TextToSpeech.getSupportedLanguages();
+    return languages;
+  }
+
+  static async getSupportedVoices() {
+    const voices = await TextToSpeech.getSupportedVoices();
+    return voices;
+  }
+
+  static async isLanguageSupported(lang) {
+    const isSupported = await TextToSpeech.isLanguageSupported({ lang });
+    return isSupported;
   }
 }
